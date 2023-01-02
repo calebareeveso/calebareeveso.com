d3680722a35b0967f41428527ffb7a96e3093b84
caleb-007 <calebareeveso7@gmail.com>
2023-01-02 23:29:18 +0000

    feat: remove all head and meta tags

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,6 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Caleb Areeveso" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <main>My personal website</main>
     </>
   );
