60a93f2b68b52a412d07290dafee5bc998e6405d
caleb-007 <calebareeveso7@gmail.com>
2023-01-03 17:01:14 +0000

    feat: set-up tailwind css

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "@next/font/google";
-// import styles from '../styles/Home.module.css'
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -63,25 +59,28 @@ export default function Home() {
         <meta
           property="og:image"
           content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        >
-          {/* Twitter */}
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content="https://metatags.io/" />
-          <meta
-            property="twitter:title"
-            content="Caleb Areeveso — Front-end Engineer "
-          />
-          <meta
-            property="twitter:description"
-            content="Front-end Engineer - Developing web-based software with a lot of depth and detail in terms of both visuals and functionality"
-          />
-          <meta
-            property="twitter:image"
-            content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-          />
-        </meta>
+        />
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta
+          property="twitter:title"
+          content="Caleb Areeveso — Front-end Engineer "
+        />
+        <meta
+          property="twitter:description"
+          content="Front-end Engineer - Developing web-based software with a lot of depth and detail in terms of both visuals and functionality"
+        />
+        <meta
+          property="twitter:image"
+          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+        />
       </Head>
-      <main>My personal website</main>
+      <main>
+        <header>
+          <h1 className="text-3xl">My personal website</h1>
+        </header>
+      </main>
     </>
   );
 }
