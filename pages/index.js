39eccf1f5727ea6ff0697dea7a8fb99739ff8052
caleb-007 <calebareeveso7@gmail.com>
2023-01-03 17:35:19 +0000

    feat: remove main tag in index.js

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,9 @@ import { Layout } from "../components/index";
 export default function Home() {
   return (
     <Layout>
-      <main>
-        <header>
-          <h1 className="text-3xl"> {">"} CALEB AREEVESO [England, UK]</h1>
-        </header>
-      </main>
+      <header>
+        <h1 className="text-3xl"> {">"} CALEB AREEVESO [England, UK]</h1>
+      </header>
     </Layout>
   );
 }
