4cf64fd263ab5ffa09b7feccd58ec62b20fd6ada
caleb-007 <calebareeveso7@gmail.com>
2023-01-04 14:52:40 +0000

    fix: remove margin-left from header image

--- components/header/index.js
+++ components/header/index.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Index(props) {
   return (
     <header className="mt-10 sm:mt-20">
-      <div className="h-[2rem] w-[2rem] mb-2 sm:mb-3 sm:w-[2rem] sm:h-[2rem] -ml-1 relative">
+      <div className="h-[2rem] w-[2rem] mb-2 sm:mb-3 sm:w-[2rem] sm:h-[2rem] relative">
         <Image
           src={"/static/images/calebareveso_avatar.png"}
           layout={"fill"}
