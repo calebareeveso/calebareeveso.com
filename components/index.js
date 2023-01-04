b5e1a8e0967c5301344cc52c4d94dc15c4a0d45a
caleb-007 <calebareeveso7@gmail.com>
2023-01-04 15:05:22 +0000

    feat: add project card component to layout

--- components/index.js
+++ components/index.js
@@ -1,4 +1,5 @@
 export { default as Header } from "./header";
 export { default as About } from "./about";
 export { default as Projects } from "./projects";
+export { default as Cards } from "./projects/cards";
 export { default as Layout } from "./layout";
